42f99f2d046d5a386418c9d7d539073b09dea936
250300G <250300g@gmail.com>
2026-05-07 19:59:15 +0200

    Ajout de index.js pour déploiement Render

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const path = require('path');
+const middlewares = jsonServer.defaults();
+
+// Ici, on pointe directement sur db.json car il est à la racine sur ton GitHub
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+
+const port = process.env.PORT || 3001;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+    console.log(`Backend live sur le port ${port}`);
+});
