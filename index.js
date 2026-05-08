2fcd7d8aadcef9e6a07546415f8faee52afcec1f
250300G <250300g@gmail.com>
2026-05-08 11:04:37 +0200

    nettoyage

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const path = require('path');
+const path = require("path");
 const middlewares = jsonServer.defaults();
 
 // Ici, on pointe directement sur db.json car il est à la racine sur ton GitHub
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 
 const port = process.env.PORT || 3001;
 
@@ -12,5 +12,5 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port, () => {
-    console.log(`Backend live sur le port ${port}`);
+  console.log(`Backend live sur le port ${port}`);
 });
